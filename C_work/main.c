ca603f9a465472e92ffb6078fef55cbb829420d6
dayeon1201 <diddid6912@naver.com>
2022-10-03 15:19:12 +0900

    Initial Commit

---
+++ C_work/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_work
+//
+//  Created by 양다연 on 2022/10/03.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
